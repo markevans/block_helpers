a1b991d19964eb90a894e3a0b9b7c8eb2f34c666
Mark Evans <mark@new-bamboo.co.uk>
2009-07-19 21:13:46 +0100

    Got rid of unnecessary directory structure

--- lib/block_helpers.rb
+++ lib/block_helpers.rb
@@ -1,3 +1,33 @@
-require 'active_support'
-require 'actionpack'
-require 'block_helpers/block_helpers'
+require 'activesupport'
+require 'action_view'
+
+module BlockHelpers
+  
+  class BlockHelper
+    
+    def self.inherited(klass)
+      # Define the helper method
+      # e.g. for a class:
+      #   class HelloHelper < BlockHelpers::BlockHelper
+      #     #.....
+      #   end
+      #
+      # then we define a helper method 'hello_helper'
+      #
+      method_name = klass.name.split('::').last.underscore
+      klass.parent.class_eval %(      
+        def #{method_name}(&block)
+          renderer = #{klass.name}.new
+          if renderer.respond_to? :render
+            concat "asdf"
+#            concat renderer.render(capture(renderer, &block))
+          else
+            block.call(renderer)
+          end
+        end
+      )
+    end
+    
+  end
+  
+end
